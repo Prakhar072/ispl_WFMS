08cc892e3c6d638af5d79c653bc85efa36a21ed5
Prakhar <f20220426@pilani.bits-pilani.ac.in>
2024-06-27 10:42:37 +0530

    frontend complete except footers

--- WORK/HR/int_appl.php
+++ WORK/HR/int_appl.php
@@ -1,59 +1,61 @@
 <?php require_once('../common/header.php') ?>
-  <div style="width: 1377px; height: 853px; left: 31px; top: 134px; position: absolute; background: white; box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.25) solid"></div>
-  <div style="width: 1298px; height: 90px; left: 62px; top: 331px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.50) solid"></div>
-  <div style="width: 1298px; height: 90px; left: 62px; top: 429px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.50) solid"></div>
-  <div style="width: 1298px; height: 90px; left: 62px; top: 527px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.50) solid"></div>
-  <div style="width: 1298px; height: 90px; left: 62px; top: 625px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.50) solid"></div>
-  <div style="width: 1298px; height: 90px; left: 62px; top: 723px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.50) solid"></div>
-  <div style="left: 1040px; top: 77px; position: absolute; color: black; font-size: 32px; font-family: Inter; font-weight: 600; word-wrap: break-word">Internal Position Requests</div>
-  <div style="left: 90px; top: 362px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">1</div>
-  <div style="left: 90px; top: 460px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2</div>
-  <div style="left: 90px; top: 558px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">3</div>
-  <div style="left: 90px; top: 656px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">4</div>
-  <div style="left: 90px; top: 754px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">5</div>
-  <div style="left: 67px; top: 294px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">S No.</div>
-  <div style="left: 503px; top: 294px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">First Name</div>
-  <div style="left: 676px; top: 294px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Last Name</div>
-  <div style="left: 521px; top: 362px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Prakhar</div>
-  <div style="left: 521px; top: 460px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Shobhit </div>
-  <div style="left: 521px; top: 558px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Riddhi</div>
-  <div style="left: 521px; top: 656px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Aryan </div>
-  <div style="left: 521px; top: 754px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">ABC</div>
-  <div style="left: 703px; top: 362px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Mittal</div>
-  <div style="left: 703px; top: 460px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Jha</div>
-  <div style="left: 703px; top: 558px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Garg</div>
-  <div style="left: 703px; top: 656px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Arora</div>
-  <div style="left: 703px; top: 754px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">XYZ</div>
-  <div style="left: 910px; top: 363px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Team Lead</div>
-  <div style="left: 910px; top: 363px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Team Lead</div>
-  <div style="left: 1206px; top: 363px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">5</div>
-  <div style="left: 914px; top: 460px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Design</div>
-  <div style="left: 914px; top: 460px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Design</div>
-  <div style="left: 1206px; top: 461px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">4</div>
-  <div style="left: 914px; top: 558px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">SDE1 </div>
-  <div style="left: 914px; top: 558px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">SDE1 </div>
-  <div style="left: 1206px; top: 559px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">7 </div>
-  <div style="left: 914px; top: 656px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">SDE2</div>
-  <div style="left: 914px; top: 656px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">SDE2</div>
-  <div style="left: 1206px; top: 657px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2</div>
-  <div style="left: 914px; top: 754px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Testing</div>
-  <div style="left: 914px; top: 754px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Testing</div>
-  <div style="left: 1206px; top: 755px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">6</div>
-  <div style="left: 920px; top: 294px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Role</div>
-  <div style="left: 920px; top: 294px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Role</div>
-  <div style="left: 1133px; top: 294px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Experience (Y)</div>
+<div style="left: 1040px; top: 77px; position: absolute; color: black; font-size: 32px; font-family: Inter; font-weight: 600; word-wrap: break-word">Internal Position Requests</div>
+  <div style="width: 1377px; height: 853px; left: 5px; top: 134px; position: absolute; background: white; box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.25) solid">
 
-  <div style="left: 216px; top: 362px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2022B3A70426P</div>
-  <div style="left: 216px; top: 460px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2022A8PS1805H</div>
-  <div style="left: 216px; top: 558px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2022B3A70426P</div>
-  <div style="left: 216px; top: 656px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2022B3A70426P</div>
-  <div style="left: 216px; top: 754px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2022B3A70426P</div>
-  <div style="left: 233px; top: 294px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Employee Code</div>
-  <input type="button" style="width: 334px; height: 69px; left: 1035px; top: 889px; position: absolute; background: #1D8AA1; border-radius: 44px">
-  <div style="left: 1075px; top: 904px; position: absolute; color: white; font-size: 32px; font-family: Inter; font-weight: 400; word-wrap: break-word">Fill vacant positions</div>
-  <div style="left: 55px; top: 148px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 36px; font-family: Inter; font-weight: 500; word-wrap: break-word">Project XYZ</div>
-  <div style="left: 62px; top: 199px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Electronics product deal with NVIDIA </div>
-  <a href="https://www.youtube.com/"><div style="left: 59px; top: 941px; position: absolute; color: #1D8AA1; font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Back</div></a>
+  
+ <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css" />
+</head>
+<body>
+
+<div class="card-container-long" style="width: 1377px;
+    height: 853px;
+    /*overflow-y: auto;*/
+    left:50px;
+    top: 500px;
+    background: white; 
+    box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25); 
+    border: 1px rgba(0, 0, 0, 0.25) solid;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 80px 600px;
+    row-gap: 20px;
+    padding: 25px;">
+    <div>
+      <div style="position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 36px; font-family: Inter; font-weight: 500; word-wrap: break-word">Project XYZ</div>
+      <br>
+  <div style="text-align:left; padding-top:25px; position: relative; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Electronics product deal with NVIDIA </div>
+</div>
+<div>
+  <table id="myTable" class="display" style="float:center; margin-top:50px">
+        <thead>
+            <tr>
+                <th>Column 1</th>
+                <th>Column 2</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Row 1 Data 1</td>
+                <td>Row 1 Data 2</td>
+            </tr>
+            <tr>
+                <td>Row 2 Data 1</td>
+                <td>Row 2 Data 2</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<input type="button" Value="Fill Vacant Positions" style=" font-size: 32px; color: white; position: absolute; top:800px; justify-self:right; width: 334px; height: 69px; background: #1D8AA1; border-radius: 44px">
+<a href="https://www.youtube.com/"><div style="left: 59px; top: 851px; position: absolute; color: #1D8AA1; font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Back</div></a>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
+    <script>
+    $(document).ready( function () {
+        $('#myTable').DataTable();
+    } );</script>
+  
+
+  
   </div>
 </div>
 <?php require_once('../common/footer.php') ?>
--- WORK/HR/notifications.php
+++ WORK/HR/notifications.php
@@ -1,201 +1,59 @@
 <?php require_once('../common/header.php') ?>
+<div style="left: 1250px; top: 77px; position: absolute; color: black; font-size: 32px; font-family: Inter; font-weight: 600; word-wrap: break-word">Notifications</div>
+  <div style="width: 1377px; height: 853px; left: 5px; top: 134px; position: absolute; background: white; box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.25) solid">
 
-<div style="left: 1199px; top: 81px; position: absolute; color: black; font-size: 32px; font-family: Inter; font-weight: 600; word-wrap: break-word">Notifications</div>
-<!--<a href="https://www.youtube.com/"><div style="left: 71px; top: 954px; position: absolute; color: #1D8AA1; font-size: 20px; font-family: Inter; font-weight: 400; word-wrap: break-word">Load Page 2</div></a>-->
+  
+ <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css" />
+</head>
+<body>
 
-<div style="left:7px; top:135px; position:absolute; height:800px">
-  
-  <!--<div class="card-container-long">
-    <div>-->
-      <div style="float:left; width:1300px; padding-left:4px; padding-bottom:14px; text-align:start; color: black; font-size: 32px; font-family: Inter; font-weight: 600; word-wrap: break-word">System</div>
-      <div style="float:right; width:30px; padding-left:4px; text-align:end; color: black; font-size: 32px; font-family: Inter; font-weight: 600; word-wrap: break-word">5</div>
-      <input class="searchbar" style=" width:450px; height:30px; float:left; padding-left: 18px; margin-bottom:18px;" type="text" placeholder="Search by Name, ID or Department">
-     
-      
-      
-  
-  
-      <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css" />
-      <div><table id="myTable"><thead>
-  <tr>
-    <th>S no.</th>
-    <th>Employee Code</th>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Sent At</th>
-    <th>Action</th>
-  </tr></thead>
-<tbody>
-  <tr>
-    <td>1</td>
-    <td>2022B3A70426P</td>
-    <td>PM</td>
-    <td>MP</td>
-    <td>3/5/12</td>
-    <td>EMAIL</td>
-  </tr>
-  <tr>
-    <td>2</td>
-    <td>2022B3A70426P</td>
-    <td>PM</td>
-    <td>MP</td>
-    <td>3/5/12</td>
-    <td>EMAIL</td>
-  </tr>
-  <tr>
-    <td>3</td>
-    <td>2022B3A70426P</td>
-    <td>PM</td>
-    <td>MP</td>
-    <td>3/5/12</td>
-    <td>EMAIL</td>
-  </tr>
-  <tr>
-    <td>4</td>
-    <td>2022B3A70426P</td>
-    <td>PM</td>
-    <td>MP</td>
-    <td>3/5/12</td>
-    <td>EMAIL</td>
-  </tr>
-  <tr>
-    <td>5</td>
-    <td>2022B3A70426P</td>
-    <td>PM</td>
-    <td>MP</td>
-    <td>3/5/12</td>
-    <td>EMAIL</td>
-  </tr>
-  <tr>
-    <td>6</td>
-    <td>2022B3A70426P</td>
-    <td>PM</td>
-    <td>MP</td>
-    <td>3/5/12</td>
-    <td>EMAIL</td>
-  </tr>
-</tbody></table></div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<div class="card-container-long" style="width: 1377px;
+    height: 853px;
+    /*overflow-y: auto;*/
+    left:50px;
+    top: 500px;
+    background: white; 
+    box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25); 
+    border: 1px rgba(0, 0, 0, 0.25) solid;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 80px 600px;
+    row-gap: 20px;
+    padding: 25px;">
+    <div>
+      <div style="position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 36px; font-family: Inter; font-weight: 500; word-wrap: break-word">System</div>
+      <br>
+</div>
+<div>
+  <table id="myTable" class="display" style="float:center; margin-top:50px">
+        <thead>
+            <tr>
+                <th>Column 1</th>
+                <th>Column 2</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Row 1 Data 1</td>
+                <td>Row 1 Data 2</td>
+            </tr>
+            <tr>
+                <td>Row 2 Data 1</td>
+                <td>Row 2 Data 2</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<a href="https://www.youtube.com/"><div style="left: 59px; top: 851px; position: absolute; color: #1D8AA1; font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Load Page 2</div></a>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
     <script>
     $(document).ready( function () {
         $('#myTable').DataTable();
     } );</script>
- 
+  
 
-  <!--</div>
-  </div>-->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--<div style="left: 99px; top: 291px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">1</div>
-<div style="left: 86px; top: 153px; position: absolute; color: rgba(0, 0, 0, 0.50); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Search by Name, Department or ID</div>
-  <div style="width: 1298px; height: 90px; left: 71px; top: 260px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.50) solid"></div>
-  <div style="width: 636px; height: 44px; left: 67px; top: 145px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 48px; border: 1px rgba(0, 0, 0, 0.50) solid"></div>
-  <div style="width: 1298px; height: 90px; left: 71px; top: 358px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.50) solid"></div>
-  <div style="width: 1298px; height: 90px; left: 71px; top: 848px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.50) solid"></div>
-  <div style="width: 1298px; height: 90px; left: 71px; top: 456px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.50) solid"></div>
-  <div style="width: 1298px; height: 90px; left: 71px; top: 554px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.50) solid"></div>
-  <div style="width: 1298px; height: 90px; left: 71px; top: 652px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.50) solid"></div>
-  <div style="width: 1298px; height: 90px; left: 71px; top: 750px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.50) solid"></div>
-  <div style="left: 99px; top: 389px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2</div>
-  <div style="left: 99px; top: 487px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">3</div>
-  <div style="left: 99px; top: 585px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">4</div>
-  <div style="left: 99px; top: 683px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">5</div>
-  <div style="left: 99px; top: 781px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">6</div>
-  <div style="left: 99px; top: 879px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">7</div>
-  <div style="left: 76px; top: 223px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">S No.</div>
-  <div style="left: 435px; top: 223px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">First Name</div>
-  <div style="left: 608px; top: 223px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Last Name</div>
-  <div style="left: 453px; top: 291px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Prakhar</div>
-  <div style="left: 986px; top: 292px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Created New Project</div>
-  <div style="left: 1006px; top: 390px; position: absolute; text-align: center; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Submitted a Task</div>
-  <div style="left: 979px; top: 489px; position: absolute; text-align: center; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Added New Employee</div>
-  <div style="left: 987px; top: 583px; position: absolute; text-align: center; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Created New Project</div>
-  <div style="left: 986px; top: 682px; position: absolute; text-align: center; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Created New Project</div>
-  <div style="left: 987px; top: 781px; position: absolute; text-align: center; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Created New Project</div>
-  <div style="left: 987px; top: 885px; position: absolute; text-align: center; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Created New Project</div>
-  <div style="left: 453px; top: 389px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Prakhar</div>
-  <div style="left: 453px; top: 487px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">HR</div>
-  <div style="left: 453px; top: 585px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Prakhar</div>
-  <div style="left: 453px; top: 683px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Prakhar</div>
-  <div style="left: 453px; top: 781px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Prakhar</div>
-  <div style="left: 453px; top: 879px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Prakhar</div>
-  <div style="left: 635px; top: 291px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Mittal</div>
-  <div style="left: 635px; top: 389px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Mittal</div>
-  <div style="left: 635px; top: 487px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">HR</div>
-  <div style="left: 635px; top: 585px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Mittal</div>
-  <div style="left: 635px; top: 683px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Mittal</div>
-  <div style="left: 635px; top: 781px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Mittal</div>
-  <div style="left: 635px; top: 879px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Mittal</div>
-  <div style="left: 790px; top: 291px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2/4 12:00</div>
-  <div style="left: 789px; top: 584px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2/4 12:00</div>
-  <div style="left: 790px; top: 389px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">4/5 14:00</div>
-  <div style="left: 789px; top: 682px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">4/5 14:00</div>
-  <div style="left: 790px; top: 487px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">1/5 19:00</div>
-  <div style="left: 789px; top: 780px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">1/5 19:00</div>
-  <div style="left: 789px; top: 885px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">1/5 19:00</div>
-  <div style="left: 803px; top: 223px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Sent At</div>
-  <div style="left: 1068px; top: 222px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Action</div>
- 
-  <div style="left: 182px; top: 291px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2022B3A70426P</div>
-  <div style="left: 182px; top: 389px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2022B3A70426P</div>
-  <div style="left: 182px; top: 487px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">HR</div>
-  <div style="left: 182px; top: 585px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2022B3A70426P</div>
-  <div style="left: 182px; top: 683px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2022B3A70426P</div>
-  <div style="left: 182px; top: 781px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2022B3A70426P</div>
-  <div style="left: 182px; top: 879px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">2022B3A70426P</div>
-  <div style="left: 191px; top: 223px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Employee Code</div>-->
-
+  
+  </div>
+</div>
 <?php require_once('../common/footer.php') ?>
