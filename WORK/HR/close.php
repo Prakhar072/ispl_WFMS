cea3749891738b61d9b16acb876df2da77e28082
Prakhar <f20220426@pilani.bits-pilani.ac.in>
2024-07-08 11:47:58 +0530

    finished all hr modules

--- WORK/HR/close.php
+++ WORK/HR/close.php
@@ -3,28 +3,46 @@
 
 // 
 
-if (isset($_POST['save_details'])) {
+if (isset($_POST['close_request'])) {
+		$created_by  	 = $_SESSION['user_id'];
+		$date_time       = date('Y-m-d');
+		$selected     	 = $_POST['dog_names'];
+		$count_sel       = count($selected);
+		$reqd_left  	 = $user->no_reqd - $count_sel;
+		$status          = "to be changed";
+		$string_product = implode(',', $selected);
 
-		$assignee  	 = $_POST['employee_id'];
-		$last_name       = $_POST['last_name'];
-		$gender  		 = $_POST['gender'];
-		$phone           = $_POST['phone'];
-		$dob  			 = $_POST['dob'];
-		$doj  			 = $_POST['doj'];
-		$email  		 = $_POST['email'];
-        $employee_code   = $_POST['employee_code'];
-		$department  	 = $_POST['department'];
-        $location  		 = $_POST['location'];
-		$position  		 = $_POST['position'];
-        $base_salary  	 = $_POST['base_salary'];
-		$bonus  		 = $_POST['bonus'];
-		$benefits  		 = $_POST['benefits'];
-		$stocks  		 = $_POST['stocks'];
-		$ctc  			 = $base_salary + $bonus + $benefits + $stocks;
-		$manager  		 = $_POST['manager'];
-		$password  		 = md5($_POST['password']);
-		$created_by      = $_SESSION['user_id'];
-		$created_on      = date('Y-m-d');
+		if ($reqd_left==0){
+			$statement1 = 'UPDATE position_request SET no_reqd="'.$reqd_left.'", status="Closed" WHERE request_id='.$user->request_id;
+			$run_query = mysqli_query($db_connect,$statement1);
+			$statement10 = 'UPDATE project SET members_id="'.$string_product.'" WHERE project_id='.$user->project_id;
+			$run_query10 = mysqli_query($db_connect,$statement10);
+			echo "request closed";
+
+			$created_by      = $_SESSION['user_id'];
+			$created_on      = date('Y-m-d');
+			$creator = $_SESSION['user_id'];
+
+			$statement5 = 'INSERT INTO `notifications`(`user`,`type`,`date_time`, `affected`, `summary`)
+			values("'.$creator.'","Filled Position Request","'.$created_on.'","'.$user->employee_id.'","Closed Request Completely")';
+			$run_query3 = mysqli_query($db_connect,$statement5);
+		}
+		else if ($reqd_left<0){
+			echo "Error: Too many added, Allowed $user->no_reqd; Actual $count_sel";
+		}
+		else{
+			$statement7 = 'UPDATE position_request SET no_reqd="'.$reqd_left.'" WHERE request_id='.$user->request_id;
+			$run_query = mysqli_query($db_connect,$statement7);
+			echo "Flled, request still open";
+
+			$created_by      = $_SESSION['user_id'];
+			$created_on      = date('Y-m-d');
+			$creator = $_SESSION['user_id'];
+
+			$statement9 = 'INSERT INTO `notifications`(`user`,`type`,`date_time`, `affected`, `summary`)
+			values("'.$creator.'","Filled Position Request","'.$created_on.'","'.$user->employee_id.'","Closed Request Partially")';
+			$run_query3 = mysqli_query($db_connect,$statement9);
+		}
 
 }
 ?>
--- WORK/HR/fill_request.php
+++ WORK/HR/fill_request.php
@@ -19,7 +19,7 @@ require_once('../common/header.php') ?>
   $query2 = mysqli_query($db_connect,$statement2);
   $count = $query2->num_rows;
   ?>
-    <select style='padding-left: 5px; width: 505px; height: 90px; left: 71px; top: 556px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.8) solid; font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word;' name='dog-names' id='dog-names'>
+    <select style='padding-left: 5px; width: 505px; height: 90px; left: 71px; top: 556px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.8) solid; font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word;' name='dog_names[]' id='dog_names' multiple='multiple' >
   <?php while ($result = mysqli_fetch_array($query2)){
     echo "
     <option style= 'color: rgba(0, 0, 0, 0.50);' value='' disabled selected hidden>Select Available Employee</option>
@@ -27,10 +27,7 @@ require_once('../common/header.php') ?>
   }
     ?>
 </select>
-<input  type="button" value="Close Fill Request" style="left: 1075px; top: 904px; position: absolute; color: white; font-size: 32px; font-family: Inter; font-weight: 400; word-wrap: break-word; width: 502px; height: 69px; left: 71px; top: 862px; position: absolute; background: #1D8AA1; border-radius: 44px">
-<div style="width: 61px; height: 61px; padding: 7.62px; left: 612px; top: 570px; position: absolute; justify-content: center; align-items: center; display: inline-flex">
-    <img src="../common/ima/Squared_plus.svg.png" style="position: relative; left:-15px; top:-5px; width: 60px; height: 60px;">
-</div>
+<input  name="close_request" type="submit" value="Close Fill Request" style="left: 1075px; top: 904px; position: absolute; color: white; font-size: 32px; font-family: Inter; font-weight: 400; word-wrap: break-word; width: 502px; height: 69px; left: 71px; top: 862px; position: absolute; background: #1D8AA1; border-radius: 44px">
 </form>
 
   <!--<input type="dropbtn" style="width: 505px; height: 90px; left: 71px; top: 556px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border: 1px rgba(0, 0, 0, 0.50) solid">
@@ -42,7 +39,7 @@ require_once('../common/header.php') ?>
   <div style="left: 327px; top: 178px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word"><?php echo $user->position_id?></div>
   <div style="left: 327px; top: 231px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word"><?php echo $user->no_reqd ?></div>
   <div style="left: 327px; top: 284px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Finance</div>
-  <div style="left: 327px; top: 337px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word"><?php echo $user->created_on ?></div>
+  <div style="left: 327px; top: 337px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word"><?php echo $user->date_time ?></div>
   <div style="left: 1195px; top: 178px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 1000; word-wrap: break-word">In Progress</div>
   <div style="left: 76px; top: 231px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Required No:</div>
   <div style="left: 77px; top: 284px; position: absolute; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Department:</div>
